a3f4224afc7dba74e1e9bc131c20544721514143
Reio Piller <reio@heathmont.net>
2022-01-20 19:13:43 +0200

    fix: release setup

--- mix.exs
+++ mix.exs
@@ -1,20 +1,23 @@
 defmodule LocalCluster.MixProject do
   use Mix.Project
 
-  @version "1.1.0"
   @url_docs "http://hexdocs.pm/local_cluster"
   @url_github "https://github.com/whitfin/local-cluster"
 
   def project do
+    version = version()
+
     [
       app: :local_cluster,
       name: "LocalCluster",
       description: "Easy local cluster creation to aid in unit testing",
       package: %{
+        organization: "coingaming",
         files: [
           "lib",
           "mix.exs",
-          "LICENSE"
+          "LICENSE",
+          "VERSION"
         ],
         licenses: [ "MIT" ],
         links: %{
@@ -23,12 +26,12 @@ defmodule LocalCluster.MixProject do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: @version,
+      version: version,
       elixir: "~> 1.2",
       deps: deps(),
       docs: [
         main: "LocalCluster",
-        source_ref: "v#{@version}",
+        source_ref: "v#{version}",
         source_url: @url_github,
       ],
       aliases: [
@@ -43,6 +46,13 @@ defmodule LocalCluster.MixProject do
       applications: [:logger],
       extra_applications: [:logger]
     ]
+  end
+
+  defp version do
+    case File.read("VERSION") do
+      {:ok, v} -> String.trim(v)
+      {:error, _} -> "0.0.0-development"
+    end
   end
 
   # Run "mix help deps" to learn about dependencies.
